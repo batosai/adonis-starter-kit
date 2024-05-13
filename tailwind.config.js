d5db51182ff7947172c40508b1c66efb7b03a538
jeremy <jeremy@chaufourier.fr>
2024-05-13 23:56:22 +0200

    feat: init template (@jrmc/edge-components)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+import edgeComponent from '@jrmc/edge-components/tailwind.config'
+
+export default {
+  content: ['./resources/views/**/*.edge', ...edgeComponent.content],
+  theme: {
+    extend: {},
+  },
+  plugins: [...edgeComponent.plugins],
+}
+

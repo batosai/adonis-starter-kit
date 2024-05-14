acafcb518c205f1c52af1b76d80a9798f759f94e
jeremy <jeremy@chaufourier.fr>
2024-05-14 23:44:13 +0200

    feat: create session

---
+++ app/controllers/session_controller.ts
@@ -0,0 +1,29 @@
+import { HttpContext } from '@adonisjs/core/http'
+import { DateTime } from 'luxon'
+import User from '#models/user'
+
+export default class SessionController {
+  async create({ view }: HttpContext) {
+    return view.render('pages/auth/login')
+  }
+
+  async store({ request, auth, session, response }: HttpContext) {
+    const { email, password } = request.only(['email', 'password'])
+
+    const user = await User.verifyCredentials(email, password)
+
+    await auth.use('web').login(user)
+
+    user.lastLoginAt = DateTime.local()
+    await user.save()
+
+    session.flash('success.message', 'success')
+
+    response.redirect('/')
+  }
+
+  async destroy({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+    response.redirect('/auth/login')
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,21 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+
+const SessionController = () => import('#controllers/session_controller')
+
 
 router.on('/').render('pages/home')
+
+router.group(() => {
+  router.get('login', [SessionController, 'create']).as('session.create')
+  router.post('login', [SessionController, 'store']).as('session.store')
+})
+  .prefix('auth')
+  .as('auth')
+  .use(middleware.guest())
+
+  router.post('auth/logout', [SessionController, 'destroy'])
+  .as('auth.session.destroy')
+  .use(middleware.auth())

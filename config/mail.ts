e49e2f9f1b1c2c6888f5f96465d3f83339469086
jeremy <jeremy@chaufourier.fr>
2024-05-17 00:33:42 +0200

    feat: add mail and i18n

---
+++ config/mail.ts
@@ -0,0 +1,36 @@
+import env from '#start/env'
+import app from '@adonisjs/core/services/app'
+import { defineConfig, transports } from '@adonisjs/mail'
+
+const mailConfig = defineConfig({
+  default: 'smtp',
+
+   /**
+    * The mailers object can be used to configure multiple mailers
+    * each using a different transport or same transport with different
+    * options.
+   */
+  mailers: {
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST'),
+      port: env.get('SMTP_PORT'),
+      ignoreTLS: app.inDev,
+			/**
+       * Uncomment the auth block if your SMTP
+       * server needs authentication
+       */
+      /* auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME'),
+        pass: env.get('SMTP_PASSWORD'),
+      }, */
+    }),
+
+  },
+})
+
+export default mailConfig
+
+declare module '@adonisjs/mail/types' {
+  export interface MailersList extends InferMailers<typeof mailConfig> {}
+}

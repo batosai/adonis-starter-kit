fddd71cc5e8978dadf9729e8beaa21009cbef58a
jeremy <jeremy@chaufourier.fr>
2024-06-05 00:15:27 +0200

    Update logger.ts

--- config/logger.ts
+++ config/logger.ts
@@ -11,7 +11,7 @@ const loggerConfig = defineConfig({
    */
   loggers: {
     app: {
-      enabled: true,
+      enabled: app.inDev,
       name: env.get('APP_NAME'),
       level: env.get('LOG_LEVEL'),
       transport: {

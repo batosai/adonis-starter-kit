9ed6daced41558739eec4b953522b27ee6e3ec46
jeremy <jeremy@chaufourier.fr>
2024-06-13 00:42:41 +0200

    feat(audit): add user fullname

--- config/auditing.ts
+++ config/auditing.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@stouder-io/adonis-auditing'
 export default defineConfig({
   userResolver: () => import('#core/audit_resolvers/user_resolver'),
   resolvers: {
+    user_fullname: () => import('#core/audit_resolvers/user_fullname_revolver'),
     ip_address: () => import('#core/audit_resolvers/ip_address_resolver'),
     user_agent: () => import('#core/audit_resolvers/user_agent_resolver'),
     url: () => import('#core/audit_resolvers/url_resolver'),

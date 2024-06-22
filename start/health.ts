643b4eb7b3bb2cc66856a54237223d3e36ea60be
jeremy <jeremy@chaufourier.fr>
2024-06-22 11:35:55 +0200

    feat: add health

---
+++ start/health.ts
@@ -0,0 +1,6 @@
+import { HealthChecks, DiskSpaceCheck, MemoryHeapCheck } from '@adonisjs/core/health'
+
+export const healthChecks = new HealthChecks().register([
+  new DiskSpaceCheck(),
+  new MemoryHeapCheck(),
+])
--- start/routes.ts
+++ start/routes.ts
@@ -1,3 +1,7 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home').as('home')
+
+const HealthChecksController = () => import('#core/controllers/health_checks_controller')
+
+router.get('/health', [HealthChecksController])

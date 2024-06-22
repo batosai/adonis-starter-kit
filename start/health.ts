0c032b8726db7935ce9fdafb9a15dae118e02d81
jeremy <jeremy@chaufourier.fr>
2024-06-22 11:39:04 +0200

    feat: add database health

--- start/health.ts
+++ start/health.ts
@@ -1,6 +1,9 @@
+import db from '@adonisjs/lucid/services/db'
+import { DbCheck } from '@adonisjs/lucid/database'
 import { HealthChecks, DiskSpaceCheck, MemoryHeapCheck } from '@adonisjs/core/health'
 
 export const healthChecks = new HealthChecks().register([
   new DiskSpaceCheck(),
   new MemoryHeapCheck(),
+  new DbCheck(db.connection()),
 ])

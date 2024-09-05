446957c4e346bc2d9873db4828f498fcaa39ba7a
jeremy <jeremy@chaufourier.fr>
2024-09-05 23:04:19 +0200

    feat: update user seed

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,10 +1,14 @@
+import app from '@adonisjs/core/services/app'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import Roles from '#core/enums/roles'
 import User from '#core/models/user'
+import { attachmentManager } from '@jrmc/adonis-attachment'
+import { readFile } from 'fs/promises'
 
 export default class extends BaseSeeder {
   async run() {
-    await User.create(
+    const buffer = await readFile(app.makePath('database/seeders/resources/me.jpg'))
+    const user = await User.create(
       {
         role: Roles.ADMIN,
         lastname: 'Chaufourier',
@@ -13,5 +17,8 @@ export default class extends BaseSeeder {
         password: 'secret',
       },
     )
+
+    user.avatar = await attachmentManager.createFromBuffer(buffer, 'avatar.jpg')
+    await user.save()
   }
 }

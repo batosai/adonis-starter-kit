968529d924999c402cc7956f96725aa313f2aaa1
jeremy <jeremy@chaufourier.fr>
2025-01-16 23:27:06 +0100

    chore: update dependencies

--- config/attachment.ts
+++ config/attachment.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from '@jrmc/adonis-attachment'
+import { InferConverters } from '@jrmc/adonis-attachment/types/config'
 
-export default defineConfig({
-  converters: [
-    {
-      key: 'thumbnail',
+const attachmentConfig = defineConfig({
+  converters: {
+    thumbnail: {
       converter: () => import('@jrmc/adonis-attachment/converters/image_converter'),
       options: {
         resize: 300,
         format: 'webp',
-      },
-    },
-  ],
+      }
+    }
+  }
 })
+
+export default attachmentConfig
+
+declare module '@jrmc/adonis-attachment' {
+  interface AttachmentVariants extends InferConverters<typeof attachmentConfig> {}
+}
